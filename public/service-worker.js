c66f4bd72c6fba3d4eebbde19a9580400e296ce4
Shinto PC <50198933+shintopc@users.noreply.github.com>
2025-08-12 16:51:56 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'barber-booking-pwa-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.jsx',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+});
